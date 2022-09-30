979276a2c6f2f3fc455796aa7c4fecce2618eb7f
Davina Leong <davina.leong.sy@gmail.com>
2022-09-30 10:56:34 +0800

    Done with form modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,19 @@
 console.log(`main.js loaded`)
 
 // Elements
+
+// Default Modal
 const modalDialogEl = document.querySelector(`[data-element=modal-dialog]`)
 const openModalBtnEl = document.querySelector(`[data-element=open-modal]`)
 const closeModalBtnEl = document.querySelector(`[data-element=close-modal]`)
+
+// Form Modal
+const formModalDialogEl = document.querySelector(
+  `[data-element=modal-form-dialog]`
+)
+const openFormModalBtnEl = document.querySelector(
+  `[data-element=open-form-modal]`
+)
 
 openModalBtnEl.addEventListener(`click`, (e) => {
   modalDialogEl.showModal()
@@ -11,4 +21,8 @@ openModalBtnEl.addEventListener(`click`, (e) => {
 
 closeModalBtnEl.addEventListener(`click`, (e) => {
   modalDialogEl.close()
+})
+
+openFormModalBtnEl.addEventListener(`click`, (e) => {
+  formModalDialogEl.showModal()
 })
