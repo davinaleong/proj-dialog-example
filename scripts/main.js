cb9a31dd4d1feedb1d60a11b68ce7f9ae9835e54
Davina Leong <davina.leong.sy@gmail.com>
2022-09-30 10:48:16 +0800

    WIP form modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 console.log(`main.js loaded`)
+
+// Elements
+const modalDialogEl = document.querySelector(`[data-element=modal-dialog]`)
+const openModalBtnEl = document.querySelector(`[data-element=open-modal]`)
+const closeModalBtnEl = document.querySelector(`[data-element=close-modal]`)
+
+openModalBtnEl.addEventListener(`click`, (e) => {
+  modalDialogEl.showModal()
+})
+
+closeModalBtnEl.addEventListener(`click`, (e) => {
+  modalDialogEl.close()
+})
